db68e557d887c995da1d82cfc2d7fe7c0b362ffa
John Folberth <johnfolberth@gmail.com>
2024-06-25 09:28:19 -0400

    adding output for app insights

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,26 @@
+output "app_id" {
+  description = "App ID of the Application Insights"
+  sensitive   = true
+  value       = azurerm_application_insights.this.app_id
+}
+
+output "connection_string" {
+  description = "Connection String of the Application Insights"
+  sensitive   = true
+  value       = azurerm_application_insights.this.connection_string
+}
+
+output "instrumentation_key" {
+  description = "Instrumentation Key of the Application Insights"
+  value       = azurerm_application_insights.this.instrumentation_key
+}
+
 output "name" {
   description = "Name of the Application Insights"
   value       = azurerm_application_insights.this.name
 }
 
-# Module owners should include the full resource via a 'resource' output
+# Guidance change to prohibit output of resource as an object. This will be a breaking change next major release.
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
   description = "This is the full output for the resource."
